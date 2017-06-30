b0585efd2b568a8705ab4dbd28c0685b4a334017
Kyle <kyle.c.r.fahey@gmail.com>
2017-06-30 15:16:55 -0500

    Update app.js

--- app.js
+++ app.js
@@ -43,14 +43,14 @@ tweets.on('connection', function(ws, req) {
       }
     }
     if(tweetsWithLoc.length === 40) {
-     ws.send(JSON.stringify({"location":tweetsWithLoc}))
+     ws.send({"location":tweetsWithLoc})
      tweetsWithLoc.length = 0;
     }
 
     //overall sentiment & stuff
     if(streamedTweets.length === 20) { 
       var trumpSentiment = sentiment(streamedTweets.join());
-      ws.send(JSON.stringify({"main": {"sentiment": trumpSentiment, "featuredTweet": streamedTweets[19] }}))
+      ws.send({"main": {"sentiment": trumpSentiment, "featuredTweet": streamedTweets[19] }})
       console.log(trumpSentiment)
       //clear array & start over
       streamedTweets.length = 0;
