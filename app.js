bbcd37642f0791653ca09ec03439d080e0f585a7
Kyle <kyle.c.r.fahey@gmail.com>
2017-07-02 14:13:45 -0500

    Push origin master

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ var T = new Twit({
   consumer_secret:      process.env.TWITTER_CONSUMER_SECRET,
   access_token:         process.env.TWITTER_ACCESS_TOKEN,
   access_token_secret:  process.env.TWITTER_ACCESS_SECRET,
-})
+}) 
 
 
 app.use(function(req, res, next) {
