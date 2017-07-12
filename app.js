1fbadc0033867e00070a565331ad0856ce18d57a
Kyle <kyle.c.r.fahey@gmail.com>
2017-07-11 20:31:14 -0500

    Remove clusering

--- app.js
+++ app.js
@@ -1,31 +1,3 @@
-'use strict';
-
-// Include the cluster module
-var cluster = require('cluster');
-
-// Code to run if we're in the master process
-if (cluster.isMaster) {
-
-    // Count the machine's CPUs
-    var cpuCount = require('os').cpus().length;
-
-    // Create a worker for each CPU
-    for (var i = 0; i < cpuCount; i += 1) {
-        cluster.fork();
-    }
-
-    // Listen for dying workers
-    cluster.on('exit', function (worker) {
-
-        // Replace the dead worker, we're not sentimental
-        console.log('Worker %d died :(', worker.id);
-        cluster.fork();
-
-    });
-
-// Code to run if we're in a worker process
-} else {
-
     const express = require('express');
     const app = express();
     const http = require('http')
@@ -191,5 +163,3 @@ if (cluster.isMaster) {
   server.listen(port, function listening() {
     console.log('Listening on %d', server.address().port);
   });
-
-}
